d0b31e6f82b95d34c9c767f5029554c3f00a4bab
Ozgur Ozhan <ozgur2096@gmail.com>
2023-11-28 13:28:39 +0100

    selectPlan.ts modified

--- src/components/contentPages/selectPlan/selectPlan.ts
+++ src/components/contentPages/selectPlan/selectPlan.ts
@@ -4,6 +4,21 @@ import { MainContent } from '../templates/contentTemplate';
 class SelectPlan extends MainContent {
   constructor(private title: string, infoText: string) {
     super(title, infoText, true, true);
+    const contentSectionSelectPlan = document.createElement('div');
+    contentSectionSelectPlan.innerHTML = `
+    <div class= "select-plan-option-section">
+      <div>Arcade</div>
+      <div>Advanced</div>
+      <div>Pro</div>
+    </div>
+    <div class= "select-plan-button-section">
+      <span>Monthly</span>
+      <button type="radio"></button>
+      <span>Yearly</span>
+    </div>
+    `;
+    contentSectionSelectPlan.classList.add('personal-info');
+    this.contentSection.appendChild(contentSectionSelectPlan);
   }
 }
 
