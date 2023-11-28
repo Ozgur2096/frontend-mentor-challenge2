33d4e61fb45c81d8f4064ebf28f5df4ea0ec4e9a
Ozgur Ozhan <ozgur2096@gmail.com>
2023-11-28 13:14:36 +0100

    personalInfo.ts modified

--- src/components/contentPages/personalInfo/personalInfo.ts
+++ src/components/contentPages/personalInfo/personalInfo.ts
@@ -1,8 +1,26 @@
+import './personalInfo.css';
 import { MainContent } from '../templates/contentTemplate';
 
 class PersonalInfo extends MainContent {
   constructor(private title: string, private infoText: string) {
     super(title, infoText, false, true);
+    const contentSectionPersonalInfo = document.createElement('div');
+    contentSectionPersonalInfo.innerHTML = `
+    <div class= "personal-info-input-section">
+      <label for="name">Name</label>
+      <input type="text" id="name" />
+    </div>
+    <div class= "personal-info-input-section">
+      <label for="email">Email Address</label>
+      <input type="email" id="email" />
+    </div>
+    <div class= "personal-info-input-section">
+      <label for="tel">Phone Number</label>
+      <input type="tel" id="tel" />
+    </div>
+    `;
+    contentSectionPersonalInfo.classList.add('personal-info');
+    this.contentSection.appendChild(contentSectionPersonalInfo);
   }
 }
 
